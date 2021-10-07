f805bdc6ebd2558aa141642d6fb9f984bd79b6f2
SaranyaSaral <91775614+SaranyaSaral@users.noreply.github.com>
2021-10-07 17:29:34 +0530

    Add files via upload

---
+++ PositiveorNegative.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class PositiveorNegative {
+
+	public static void main(String[] args) {
+int num=35;
+if(num>0) {
+	System.out.println("positive number is" +num);
+} 
+else 
+	if(num<0)
+			System.out.println("nagative number is" +num);
+}
+
+	}
+
+
